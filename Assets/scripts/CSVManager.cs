dbce63931725a412336c823b994a02a5455f629d
gamerfreesoul <627500966@qq.com>
2018-10-29 00:30:36 +0800

    c# csv读取

---
+++ Assets/scripts/CSVManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using CSV;
+using System.Collections.Generic;
+
+public class CSVManager : MonoBehaviour {
+
+    // Use this for initialization
+    struct CNormalData
+    {
+        public int id;
+        public string strDesc;
+    }
+    class CNormalRes : Csv<CNormalData>
+    {
+        public override bool LoadLine(Dictionary<string, string> lineInfo, ref CNormalData data)
+        {
+            data.id = int.Parse(lineInfo["ID"]);
+            data.strDesc = lineInfo["Name"];
+            return true;
+        }
+    }
+    CNormalRes data;
+    void Start () {
+        data = new CNormalRes();
+        string filePath = Application.streamingAssetsPath + "/CSVInfo.csv";
+        data.LoadCsvFile(filePath);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CNormalData value = data.FindData(4);
+        if (value.id == 0)
+            Debug.Log("Fail Value");
+        else
+            Debug.Log(value.strDesc);
+    }
+}
---
+++ Assets/scripts/Csv.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CSV
+{
+    public class Csv<T> where T : new()
+    {
+        public bool LoadCsvFile(string filePath)
+        {
+            dic = new Dictionary<int, T>();
+            string[] fileData = File.ReadAllLines(filePath);
+            string[] keys = fileData[0].Split(',');
+            for (int i = 1; i < fileData.Length; i++)
+            {
+                string[] line = fileData[i].Split(',');
+                int ID = int.Parse(line[0]);
+                Dictionary<string, string> result = new Dictionary<string, string>();
+                for (int j = 0; j < line.Length; j++)
+                    result[keys[j]] = line[j];
+                T value = new T();
+                if (!LoadLine(result, ref value))
+                    return false;
+                dic.Add(ID, value);
+            }
+            return true;
+        }
+        public virtual bool LoadLine(Dictionary<string, string> lineInfo, ref T data){ return true; }
+        public bool Find(int ID, ref T value)
+        {
+            if (!dic.ContainsKey(ID))
+                return false;
+
+            value = dic[ID];
+            return true;
+        }
+
+        public T FindData(int ID)
+        {
+            if (!dic.ContainsKey(ID))
+                return default(T);
+
+            return dic[ID];
+        }
+        Dictionary<int, T> dic;
+    }
+}
